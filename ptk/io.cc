c22c30d3fa8e4c2b6111e5636c8da5b8d9a87fb8
Andy Turk <andyturk@gmail.com>
2014-07-02 14:09:35 -0700

    significant refactoring

--- ptk/io.cc
+++ ptk/io.cc
@@ -1,12 +1,32 @@
 #include "ptk/io.h"
 
-DeviceInputBase::DeviceInputBase(uint8_t *storage, unsigned len) :
-  fifo(storage, len)
-{
+using namespace ptk;
+
+size_t DeviceInStream::read(uint8_t *buffer, size_t len) {
+  return fifo.read(buffer, len);
 }
 
+bool DeviceInStream::get(uint8_t &ch) {
+  return fifo.read(&ch, 1) == 1;
+}
 
-DeviceOutputBase::DeviceOutputBase(uint8_t *storage, unsigned len) :
-  fifo(storage, len)
-{
+size_t DeviceOutStream::write(const uint8_t *buffer, size_t len) {
+  return fifo.write(buffer, len);
+}
+
+bool DeviceOutStream::put(uint8_t ch) {
+  return fifo.write(&ch, 1) == 1;
+}
+
+USBEndpoint *USBEndpoint::eps[MAX_ENDPOINTS];
+
+void USBEndpoint::init(uint8_t ep) {
+  this->ep_id = ep;
+  eps[ep] = this;
+}
+
+void USBEndpoint::transfer_endpoint_data(uint8_t id) {
+  if (id < MAX_ENDPOINTS && eps[id]) {
+    eps[id]->transfer();
+  }
 }
--- ptk/io.h
+++ ptk/io.h
@@ -3,40 +3,104 @@
 #include "ptk/fifo.h"
 #include "ptk/ptk.h"
 
-#include <stdint.h>
+#include <cstdint>
+#include <cstddef>
+
+// Declarations
 
 namespace ptk {
-  struct DeviceInputBase {
+  struct InStream {
+    virtual size_t read(uint8_t *buffer, size_t max) = 0;
+    virtual bool get(uint8_t &ch) = 0;
+  };
+
+  struct OutStream {
+    virtual size_t write(const uint8_t *buffer, size_t len) = 0;
+    virtual bool put(uint8_t ch) = 0;
+  };
+
+  class DeviceInStream : public InStream {
+  protected:
     FIFO<uint8_t> fifo;
+    void device_wrote_to_fifo();
+
+  public:
+    DeviceInStream(uint8_t *fifo_storage, size_t fifo_size);
+    virtual size_t read(uint8_t *buffer, size_t max);
+    virtual bool get(uint8_t &ch);
+
     Event not_empty;
-
-    DeviceInputBase(uint8_t *storage, unsigned len);
-    virtual void device_read_isr() = 0;
-    virtual unsigned read(uint8_t *buffer, unsigned maxlen) = 0;
   };
 
-  struct DeviceOutputBase {
+  class DeviceOutStream : public OutStream {
+  protected:
     FIFO<uint8_t> fifo;
+    void device_read_from_fifo();
+
+  public:
+    DeviceOutStream(uint8_t *fifo_storage, size_t fifo_size);
+    virtual size_t write(const uint8_t *buffer, size_t len);
+    virtual bool put(uint8_t ch);
+
     Event not_full;
-
-    DeviceOutputBase(uint8_t *storage, unsigned len);
-    virtual void device_write_isr() = 0;
-    virtual unsigned write(const uint8_t *buffer, unsigned len) = 0;
   };
 
-  template<unsigned N>
-  class DeviceInput : public DeviceInputBase {
-    uint8_t data[N];
+  struct USBEndpoint {
+    enum {MAX_ENDPOINTS = 9};
+    static USBEndpoint *eps[MAX_ENDPOINTS];
+    uint8_t ep_id;
+
+    virtual void init(uint8_t ep);
+    virtual void transfer() = 0;
+
+    static void transfer_endpoint_data(uint8_t ep);
+  };
+
+  class EchoThread : public SubThread {
+    DeviceInStream &in;
+    OutStream &out;
 
   public:
-    DeviceInput() : DeviceInputBase(data, N) { }
-  };
+    EchoThread(DeviceInStream &in, OutStream &out) :
+      in(in), out(out)
+    { }
 
-  template<unsigned N>
-  class DeviceOutput : public DeviceOutputBase {
-    uint8_t data[N];
+    virtual void run() {
+      PTK_BEGIN();
 
-  public:
-    DeviceOutput() : DeviceOutputBase(data, N) { }
+      while (1) {
+        PTK_WAIT_EVENT(in.not_empty, TIME_INFINITE);
+
+        unsigned bytes_copied;
+        do {
+          uint8_t buf[8]; // should work with any buffer size > 0
+          
+          bytes_copied = in.read(buf, sizeof(buf));
+          out.write(buf, bytes_copied);
+        } while (bytes_copied > 0);
+      }
+
+      PTK_END();
+    }
   };
+}
+
+// Inline Definitions
+
+namespace ptk {
+  inline DeviceInStream::DeviceInStream(uint8_t *fifo_storage, size_t fifo_size) :
+    fifo(fifo_storage, fifo_size)
+  { }
+
+  inline void DeviceInStream::device_wrote_to_fifo() {
+    if (fifo.read_capacity() > 0) broadcast_event(not_empty, 0);
+  }
+
+  inline DeviceOutStream::DeviceOutStream(uint8_t *fifo_storage, size_t fifo_size) :
+    fifo(fifo_storage, fifo_size)
+  { }
+
+  inline void DeviceOutStream::device_read_from_fifo() {
+    if (fifo.write_capacity() > 0) broadcast_event(not_full, 0);
+  }
 }
